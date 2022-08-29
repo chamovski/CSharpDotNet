bedf4b1488a57dca8b006f9bfb07003743f5741b
chamovski <74669727+chamovski@users.noreply.github.com>
2022-08-29 20:21:59 +0200

    Add files via upload

---
+++ Numbers/Numbers/Program.cs
@@ -0,0 +1,56 @@
+﻿//unsigned integer means positive whole number or 0
+uint naturalNumber = 23;
+// integer means negative or positive whole number or 0
+int integerNumber = -23;
+// float means single-precision floating point
+// F suffix makes it a float literal
+float realNumber = 2.3F;
+// double means double-precision floating point
+double anotherRealNumber = 2.3; // double literal
+
+System.Console.WriteLine(naturalNumber);
+System.Console.WriteLine(integerNumber);
+System.Console.WriteLine(realNumber);
+System.Console.WriteLine(anotherRealNumber);
+
+// three variables that store the number 2 million
+int decimalNotation = 2_000_000;
+int binaryNotation = 0b_0001_1110_1000_0100_1000_0000;
+int hexadecimalNotation = 0x_001E_8480;
+
+System.Console.WriteLine($"{decimalNotation == binaryNotation}");
+System.Console.WriteLine($"{decimalNotation == hexadecimalNotation}");
+System.Console.WriteLine(decimalNotation);
+System.Console.WriteLine(binaryNotation);
+System.Console.WriteLine(hexadecimalNotation);
+
+
+System.Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range {int.MinValue:N0} to {int.MaxValue:N0}.");
+System.Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range { double.MinValue:N0} to {double.MaxValue:N0}.");
+System.Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range { decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
+
+System.Console.WriteLine("Using Doubles");
+   double  a = 0.1;
+   double  b = 0.2;
+
+if(a + b == 0.3)
+{
+    System.Console.WriteLine($"{a} + {b} equals {0.3}");
+}
+else
+{
+    System.Console.WriteLine($"{a} + {b} does NOT equals {0.3}");
+}
+
+System.Console.WriteLine("Using Decimals");
+   decimal  c = 0.1M;
+   decimal  d = 0.2M;
+
+if (c + d == 0.3M)
+{
+    System.Console.WriteLine($"{c} + {d} equals {0.3}");
+}
+else
+{
+    System.Console.WriteLine($"{c} + {d} does NOT equals {0.3}");
+}
---
+++ Numbers/Variables/Program.cs
@@ -0,0 +1,7 @@
+﻿object height = 1.88; // storing a double in an object
+object name = "Amir"; // storing a sting in an object
+Console.WriteLine($"{name} is {height} meters tall");
+
+//int length1 = name.Length; // gives compile error!
+int length2 = ((string)name).Length; // tell compiler it is a string
+Console.WriteLine($"{name} has {length2} characters.");
